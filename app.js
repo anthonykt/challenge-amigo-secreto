a83fb8512ca8473e30dd51c75a1af6bcae83266f
Anthony <anthony7.dev@gmail.com>
2025-08-16 09:11:27 -0500

    App.js

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+const amigos = [];
+
+const inputAmigo = document.getElementById('amigo');
+const listaAmigos = document.getElementById('listaAmigos');
+const resultadoEl = document.getElementById('resultado');
+
+function actualizarLista() {
+
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+
+}
+
+function agregarAmigo() {
+    const nombre = (inputAmigo.value || "").trim();
+
+    if (!nombre) {
+        alert("Por favor, inserte un nombre.");
+        inputAmigo.focus();
+        return;
+    }
+
+    amigos.push(nombre);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+
+    actualizarLista();
+
+}
+
+function sortearAmigo() {
+
+    if (amigos.length === 0) {
+        resultadoEl.innerHTML = "<li>No hay amigos para sortear.</li>"
+        return;
+    }
+
+    const indice = Math.floor(Math.random() * amigos.length);
+
+    const elegido = amigos[indice];
+
+    resultadoEl.innerHTML = `<li>🎉Tu amigo secreto es: <strong>${elegido}</strong></li>`;
+
+}
+
+inputAmigo.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') agregarAmigo();
+});
